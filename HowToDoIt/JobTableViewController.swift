faffbc39f89668b91dc0fe67d0633f8e925413cc
Henning Hoyer <henning@henninghoyer.com>
2015-07-19 22:38:25 +0200

    Added function to JobTableViewController so that we can now create and save a new job.

--- HowToDoIt/JobTableViewController.swift
+++ HowToDoIt/JobTableViewController.swift
@@ -12,7 +12,14 @@ class JobTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Set title of the view
+        title = NSLocalizedString("titleJobTableViewController", tableName: nil, bundle: NSBundle.mainBundle(), value: "Job", comment: "This is the navigation bar title")
+        
+        // Add an "Add" Button to the right side of the navigation bar
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "addJob:")
+        navigationItem.setRightBarButtonItem(addButton, animated: true)
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -20,6 +27,34 @@ class JobTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
+    // MARK: - Job Table View Handlers
+    func addJob(sender:UIBarButtonItem) {
+        
+        // When add gets pressed we need an alert to come up and ask for input. At least for now.
+        // Dialog itself
+        let dialog = UIAlertController(title: "New Job", message: "Please add the name of the Job you would like to add.", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        // Buttons to be added to dialog
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        let ok = UIAlertAction(title: "Ok", style: .Default) { (action) -> Void in
+            println("OK pressed.")
+            
+            if let textField = dialog.textFields?.first as? UITextField where !textField.text.isEmpty {
+                Job.createJobWithName(textField.text)
+            }
+        }
+        let jobTitle = dialog.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Jobname"
+        }
+        
+        // Add actions to dialog
+        dialog.addAction(cancel)
+        dialog.addAction(ok)
+        
+        // Show dialog
+        presentViewController(dialog, animated: true, completion: nil)
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
